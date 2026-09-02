4a15594a73f881660c163226a942201d3613da96
hacker <remedy.cool0@gmail.com>
2026-09-02 16:32:56 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Investor.gov | Check Out Your Investment Professional',
+  icons: {
+    icon: { url: '/logo.png', type: 'image/png' },
+    shortcut: '/logo.png',
+    apple: '/logo.png',
+  },
   description:
     'Check an investment professional’s background, registration, and disciplinary history.',
   openGraph: {
